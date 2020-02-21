be2f334aca5bcc4d3395a0e037d2ef5e0fe16e3e
InvisibleSpectator <vladislav5678@yandex.ru>
2020-02-22 01:27:02 +0400

    feat: add localisation for header

--- src/components/core/header/Header.js
+++ src/components/core/header/Header.js
@@ -2,30 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { NavPoint } from './navPoint';
 import './header.scss';
+import { withTranslation } from 'react-i18next';
+import '@src/i18n';
 
-const Header = () => (
+
+const Header = ({ t, i18n }) => (
 	<header className="header">
 		<div className="container-fluid">
 			<nav className="nav justify-content-between">
 				<Link to="/">
-					<NavPoint text="Главная страница" />
+					<NavPoint text={t('controls:mainPage')} />
 				</Link>
 				<Link to="/search">
-					<NavPoint text="Поиск по фотографам"  />
+					<NavPoint text={t('controls:search')} />
 				</Link>
 				<Link to="/team">
-					<NavPoint text="Наша команда" />
+					<NavPoint text={t('controls:team')} />
 				</Link>
 				<Link to="/styleguide">
-					<NavPoint text="Стайлгайд" />
+					<NavPoint text={t('controls:styleguide')} />
 				</Link>
 				<Link to="">
-					<NavPoint text="Ворклог"  />
+					<NavPoint text={t('controls:worklog')} />
 				</Link>
-
 			</nav>
+			<select onChange={(e) => {	i18n.changeLanguage(e.target.value);}}>
+				<option value='ru'>Пункт 1</option>
+				<option value='en'>Пункт 2</option>
+				<option value='be'>Пункт 3</option>
+			</select>
 		</div>
 	</header>
 );
 
-export default Header;
+export default withTranslation()(Header);
