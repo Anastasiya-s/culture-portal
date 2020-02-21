79a52341216b8b25d07b85f677d8639b4cf5343e
Ana_stasya_S <anastasiya-s@live.de>
2020-02-21 16:18:46 +0300

    feat: add header  with links to pages and footer

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { HomePage } from './pages/home';
 import { SearchPage } from './pages/search';
 import { StyleguidePage } from './pages/styleguide';
+import { Header } from '@core/header';
+import { Footer } from '@core/footer';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/search" component={SearchPage} />
           <Route path="/styleguide" exact component={StyleguidePage} />
           {/* <Route path="/team" component={TeamPage} />
           <Route path="/personal-info" component={PersonalPage} /> */}
-          {/* <Route path="/team" component={TeamPage} />
           {/* <Route path="/personal-info" component={PersonalPage} /> */}
         </Switch>
+        <Footer />
       </BrowserRouter>
     )
   }
