981a83526bc1033273ff14a042f9241dc4744b62
Yegor Kuzin <yahor2000@gmail.com>
2020-02-22 18:36:44 +0300

    feat: add project description structure

---
+++ src/components/pages/home/components/projectDescription/ProjectDescription.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Title1 from '@core/titles/components/title1/Title1';
+
+const ProjectDescription = () => (
+  <div>
+    <Title1 text="Culture Portal"/>
+    <div>
+      Cupidatat officia aliquip ex proident. Lorem pariatur incididunt in labore deserunt consequat laboris quis labore. Excepteur proident sint laboris nisi cupidatat sit velit ea aute aute. Commodo elit ut do et est irure. Cupidatat deserunt anim ipsum amet laboris irure.
+
+Dolor fugiat laborum mollit dolore labore esse dolor cupidatat dolore quis culpa qui. Aute dolor esse voluptate cupidatat culpa ipsum aliqua culpa. Adipisicing dolore culpa duis officia sit velit quis est do mollit id fugiat ad quis. Velit elit mollit labore anim sit. Consequat ullamco consectetur ea exercitation aliquip incididunt nostrud duis nisi adipisicing id qui aliqua. Dolor in do ullamco deserunt. Ad adipisicing dolor non nisi do nisi.
+
+Eu non ut velit proident. Tempor aliqua adipisicing ad laborum tempor qui ea elit amet incididunt aliqua sint veniam do. Laboris laboris ex fugiat ipsum amet ut enim. Officia adipisicing eu pariatur adipisicing ipsum est nisi.
+
+Laborum reprehenderit qui exercitation quis eu dolor consectetur esse eiusmod nostrud. Nulla commodo esse mollit commodo amet sint reprehenderit. Veniam do nostrud consectetur fugiat ea mollit quis sint. Deserunt sunt aliqua commodo exercitation commodo occaecat ex in in ex eiusmod dolore consequat. In voluptate pariatur est veniam commodo consequat dolor consequat. Deserunt voluptate ullamco occaecat officia magna veniam do tempor ipsum sit ex id veniam.
+    </div>
+  </div>
+);
+
+export default ProjectDescription;
