6b4d42a9991299ee559564488eb6d9c764832634
Ana_stasya_S <anastasiya-s@live.de>
2020-02-23 21:12:52 +0300

    feat: add link to authors page

--- src/components/pages/search/SearchPage.js
+++ src/components/pages/search/SearchPage.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
-import '@src/i18n';
 
 import { AuthorCard } from './components/author-card';
 
-const SearchPage = ({t}) => {
+import '@src/i18n';
+
+const SearchPage = ({ t }) => {
   const authorsArray = t('authors:authors');
   return (
-  <div>
-    Search Page
-    <ul>{authorsArray.map(author => <li key={author.id} ><AuthorCard photo={author.selfie} city={author.spawnPoint} name={author.name}/></li>)}</ul>
-  </div>
+    <div>
+      <ul>{authorsArray.map(author => 
+        <li key={author.id} >
+          <Link to={`/author/${author.id}`} >
+            <AuthorCard 
+              photo={author.selfie} 
+              city={author.spawnPoint} 
+              name={author.name}
+            />
+          </Link>
+        </li>)}
+      </ul>
+    </div>
   )
 }
 
