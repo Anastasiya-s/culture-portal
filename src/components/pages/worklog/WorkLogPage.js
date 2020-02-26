c813cfcaccc5bc0daf5a932bf5107b1277f952f4
InvisibleSpectator <vladislav5678@yandex.ru>
2020-02-26 21:42:19 +0400

    feat: add self evaluation on worklog page

--- src/components/pages/worklog/WorkLogPage.js
+++ src/components/pages/worklog/WorkLogPage.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 import { People } from './components/people';
+import {SelfEvaluation} from './components/selfEvaluation';
 
 const WorkLog = () => (
   <div className="container">
-  <People />
-   
-    
-   
+<Tabs>
+    <TabList>
+      <Tab>Ворклог</Tab>
+      <Tab>Трудности</Tab>
+      <Tab>Самопроверка</Tab>
+    </TabList>
+
+    <TabPanel>
+      <People />
+    </TabPanel>
+    <TabPanel>
+      Any content 2
+    </TabPanel>
+    <TabPanel>
+      <SelfEvaluation/>
+    </TabPanel>
+  </Tabs>
   </div>
 );
 
